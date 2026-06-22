7d7615209e1e0c9a74a04e6199dbfd73f6ca918d
NLawonn at SCF <nlawonn@student.scf.edu>
2026-06-22 17:06:33 -0400

    Add ProgrammingExercise 6 code and technical document.

--- ProgrammingExercise6/NateLawonn_ProgrammingExercise_6.py
+++ ProgrammingExercise6/NateLawonn_ProgrammingExercise_6.py
@@ -12,9 +12,7 @@ def phone_num(number):
         pattern = r'\d\d\d-\d\d\d-\d\d\d\d'
         s = input('Enter a 10-digit tel. number in format xxx-xxx-xxxx: ')
         while not re.fullmatch(pattern, s):
-            s = input("Invalid format. Please enter a phone number in xxx-xxx-xxxx format. ")
-        print('Number accepted.')
-
+            s = input("Invalid format. )
 def socsec_num(number):
     #Sets the SSN format and asks for user input
     if number == "2":
